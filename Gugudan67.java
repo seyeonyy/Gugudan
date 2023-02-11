5e165e79c8e862e28d5b2a940f2c68808d8e1049
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2023-02-12 01:07:56 +0900

    Add files via upload

---
+++ Gugudan67.java
@@ -0,0 +1,22 @@
+
+public class Gugudan67 {
+	public static void main(String[] args) {
+		int i = 1;
+		int j = 1;
+		int result7 = 0;
+		int result8 = 0;
+		System.out.println("7단");
+		while(i < 10) {
+			result7 = 7 * i;
+			System.out.println("7 x "+i+"="+result7);
+			i++;
+		}
+		System.out.println("\n8단");
+		while(j < 10) {
+			result8 = 8 * j;
+			System.out.println("8 x "+j+"="+result8);
+			j++;
+		}
+	}
+}
+
